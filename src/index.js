d62ac72ff8f18977a1fc2894d807c63ca2343434
sana2211 <70413361+sana2211@users.noreply.github.com>
2020-12-22 01:24:36 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-iimport React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import STORE from './STORE'
-import App from './App';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
 ReactDOM.render(
-  <App store={STORE} />,
+  <App />,
+
   document.getElementById('root'),
 );
 
